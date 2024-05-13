a987e79f76e9490efd6d2940dcdf731df55022a8
Zosia Samosia <Zosia@Samosia.pl>
2024-05-13 12:31:45 +0200

    add navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 'use strict';
 
-const hamburger = document.querySelector('.material-icons');
+const hamburger = document.querySelector('nav .material-icons');
+
+const close = document.querySelector('.navigation .material-icons');
+
+const menuDiv = document.querySelector('.navigation'); 
 
 hamburger.addEventListener('click', () => {
-    alert('test2')
-})
+
+    menuDiv.classList.add('showMenu')
+});
+
+close.addEventListener('click', () => {
+
+    menuDiv.classList.remove('showMenu')
+});
